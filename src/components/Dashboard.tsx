6bcd5f371e3e1b9b70239a024fb99e432e457f81
jcco-elife <juan.carlos@elifeteam.com>
2023-05-18 17:39:43 -0600

    Add logout button

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,8 +3,8 @@ import { UserContext } from '../utils/context'
 import { cardProps, infoType } from '../utils/interfaces'
 import {Card, LoadingCard} from './Card'
 import rickandmorty from './../assets/rickandmorty.jpg'
-
-const baseApi = 'https://rickandmortyapi.com/api'
+import { Link } from 'react-router-dom'
+import { baseApi } from '../utils/utils'
 
 const Dashboard:FC = () => {
   const {username} = useContext(UserContext)
@@ -30,6 +30,10 @@ const Dashboard:FC = () => {
     } 
   }
 
+  const handleLogOut = () => {
+    localStorage.removeItem('accessToken')
+  }
+
   useEffect(() => {
     fetchData(`${baseApi}/character`)
   },[])
@@ -37,7 +41,8 @@ const Dashboard:FC = () => {
   return (
     <div className='w-full h-screen m-0 overflow-hidden'>
       <nav className='w-full flex justify-end bg-gray-900 py-8 z-50 fixed left-0 top-0'>
-        <p className='text-white text-xl font-bold mr-5'>Hello {username}!</p>
+        <p className='text-white text-xl font-bold mr-10'>Hello {username}!</p>
+        <Link className='text-white text-xl mr-10' to='/' onClick={handleLogOut}>log out</Link>
       </nav>
       <div className='w-full h-full flex m-0'>
         <aside className='hidden lg:block w-96 bg-slate-300/25 shadow-xl shadow-gray-500 h-screen fixed left-0 top-0 flex z-0'>
@@ -46,7 +51,9 @@ const Dashboard:FC = () => {
         <main className='mt-40 lg:ml-96 w-full h-full overflow-auto' onScroll={handleScroll} >
             {
               characters.map((character) => (
-                <Card key={`${character.name}-${character.id}`} {...character}/>
+                <Link to={`/character/${character.id}`}>
+                  <Card key={`${character.name}-${character.id}`} {...character}/>
+                </Link>
               ))
             }
             {loading && 
