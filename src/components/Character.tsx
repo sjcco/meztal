d68238d648701380200591a7892bc2bac5f6a932
jcco-elife <juan.carlos@elifeteam.com>
2023-05-18 23:12:13 -0600

    Update layout for mobile

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -21,19 +21,19 @@ const Character:FC = () => {
 
   return (
     <div className='w-screen h-screen bg-gradient-to-r from-emerald-500 from-20% via-sky-500 via-60% to-indigo-500 to-90% flex items-center'>
-      <div className='w-5/6 lg:w-2/3 h-3/4 lg:h-2/3 bg-white rounded-3xl mx-auto flex flex-col'>
+      <div className='w-5/6 lg:w-2/3 h-5/6 lg:h-2/3 bg-white rounded-3xl mx-auto flex flex-col'>
         <Link className='text-blue-700 m-5 hover:underline ' to='/dashboard'>
           {'< back'}
         </Link>
         <div className='w-full flex flex-col lg:flex-row lg:mt-12 lg:mx-auto lg:w-10/12'>
-          <img className='rounded-3xl mx-auto mb-5' src={character?.image} alt={character?.name} />
+          <img className='rounded-3xl h-1/3 lg:h-auto mx-auto mb-3 lg:mb-5' src={character?.image} alt={character?.name} />
           <div className='w-4/6 mx-auto lg:w-3/6'>
-            <ul className='list-none text-center border-b-2 border-gray-400/75 pb-5'>
+            <ul className='list-none text-center border-b-2 border-gray-400/75 pb-3 lg:pb-5'>
               <li>{character?.name}</li>
               <li>{character?.species}</li>
               <li>{character?.origin.name}</li>
             </ul>
-            <ul className='list-none text-center border-b-2 border-gray-400/75 py-5'>
+            <ul className='list-none text-center border-b-2 border-gray-400/75 pb-3 lg:py-5'>
               <li>status: {character?.status}</li>
               <li>gender: {character?.gender}</li>
               <li>location: {character?.location.name}</li>
